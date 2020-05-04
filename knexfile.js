1498ae325435eb26c34666b6562569ea57cc17cb
Kathryn Roberts <kathrynr22@Kathryns-MacBook-Air.local>
2020-05-04 13:46:59 +0100

    amended knexfile.js to link migrations folder, created first create topic.js file

--- knexfile.js
+++ knexfile.js
@@ -12,17 +12,31 @@ const baseConfig = {
 
 const customConfig = {
   development: {
+    client: 'pg',
+    seeds: {
+      directory: './db/seeds',
+    },
     connection: {
       database: 'nc_news'
       // user,
       // password
-    }
+    },
+    migrations: {
+      directory: './db/migrations',
+    },
   },
   test: {
+    client: 'pg',
+    seeds: {
+      directory: './db/seeds',
+    },
     connection: {
       database: 'nc_news_test'
       // user,
       // password
+    },
+    migrations: {
+      directory: './db/migrations',
     }
   }
 };
