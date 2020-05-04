e11e0842c12a502d08034a0d68a704d6de7f35ec
Kathryn Roberts <kathrynr22@Kathryns-MacBook-Air.local>
2020-05-04 14:37:57 +0100

    created a topics table

--- db/migrations/20200504134550_create_topics.js
+++ db/migrations/20200504134550_create_topics.js
@@ -1,8 +1,25 @@
 
-exports.up = function(knex) {
-  
+// Each topic should have:
+
+// - `slug` field which is a unique string that acts as the table's primary key
+//   - `description` field which is a string giving a brief description of a given topic
+
+exports.up = function (knex) {
+
+  console.log('creating topics table...')
+
+  return knex.schema.createTable('topics', (topicsTable) => {
+    topicsTable.string('slug').primary();
+    topicsTable.string('description').notNullable();
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+
+  console.log('removing topics table...')
+
+  return knex.schema.dropTable('topics');
 };
+
+
+
