1a95bff7c3e7b4f35ac925ba35e34ad7cc42b7ca
Kathryn Roberts <kathrynr22@Kathryns-MacBook-Air.local>
2020-05-05 16:04:53 +0100

    added some stuff to controllers and models

---
+++ models/models.js
@@ -0,0 +1,6 @@
+const connection = require('../db/connection')
+
+exports.selectTopics = () => {
+  console.log('inside select topics models')
+  return knex.select('*').from('topics')
+}
--- routes/topicsRouter.js
+++ routes/topicsRouter.js
@@ -1,3 +1,6 @@
 const topicsRouter = require('express').Router();
+const { getAllTopics } = require('../controllers/controllers')
+
+topicsRouter.route('/').get(getAllTopics)
 
 module.exports = topicsRouter;
